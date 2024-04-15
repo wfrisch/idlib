3995ba3da8e5592d831ea7df9590966dfba1adeb
Wolfgang Frisch <wolfgang.frisch@suse.com>
2024-04-15 15:18:05 +0200

    metric: doc

--- metric.py
+++ metric.py
@@ -1,4 +1,66 @@
 #!/usr/bin/env python3
+"""
+Development tool to assist with file selection for new libraries.
+
+Example session:
+
+`./metric.py libraries/flac`
+
+```
+Score  TimeCov  CommitCov  Path
+0.082  1.000    0.082      src/libFLAC/stream_encoder.c
+0.064  0.998    0.064      src/libFLAC/stream_decoder.c
+0.063  0.978    0.064      src/flac/encode.c
+0.058  0.997    0.058      src/flac/main.c
+0.043  0.997    0.043      src/flac/decode.c
+0.028  0.967    0.029      src/libFLAC/format.c
+0.026  0.905    0.029      src/libFLAC/metadata_iterators.c
+0.025  0.965    0.026      src/libFLAC/lpc.c
+0.023  0.946    0.025      src/libFLAC/cpu.c
+0.023  0.965    0.024      include/FLAC/format.h
+0.023  0.978    0.023      include/FLAC/stream_encoder.h
+0.019  0.902    0.021      src/libFLAC/metadata_object.c
+0.019  0.965    0.019      src/test_libFLAC/metadata_manip.c
+0.018  0.965    0.019      src/test_libFLAC++/metadata_manip.cpp
+0.018  0.905    0.020      include/FLAC/metadata.h
+0.017  0.956    0.018      src/metaflac/main.c
+0.017  0.965    0.017      src/libFLAC/stream_encoder_framing.c
+0.017  0.965    0.017      src/libFLAC/include/private/lpc.h
+0.016  0.965    0.016      include/FLAC/stream_decoder.h
+0.015  0.978    0.015      src/flac/encode.h
+
+Suggested config:
+[
+    "src/libFLAC/stream_encoder.c",
+    "src/libFLAC/stream_decoder.c",
+    "src/flac/encode.c",
+    "src/flac/main.c",
+    "src/flac/decode.c",
+    "src/libFLAC/format.c",
+    "src/libFLAC/metadata_iterators.c",
+    "src/libFLAC/lpc.c",
+    "src/libFLAC/cpu.c",
+    "include/FLAC/format.h",
+    "include/FLAC/stream_encoder.h",
+    "src/libFLAC/metadata_object.c",
+    "src/test_libFLAC/metadata_manip.c",
+    "src/test_libFLAC++/metadata_manip.cpp",
+    "include/FLAC/metadata.h",
+    "src/metaflac/main.c",
+    "src/libFLAC/stream_encoder_framing.c",
+    "src/libFLAC/include/private/lpc.h",
+    "include/FLAC/stream_decoder.h",
+    "src/flac/encode.h"
+]
+```
+
+- TimeCov [0..1] describes the lifespan of a file in the repo
+- CommitCov [0..1] describes how many commits affected a file
+- Score [0..1] = TimeCov * CommitCov
+
+This is a rather simple metric and developers should review and adjust the
+suggestion manually.
+"""
 import re
 import sys
 import json
@@ -52,7 +114,7 @@ repo_path = Path(args.repo_path)
 assert repo_path.is_dir()
 git = GitRepo(repo_path)
 
-print("This typically takes a long time...")
+print("This may take a long time, depending on the repo size...")
 
 re_cc_filename = re.compile(r'.*\.(c|cc|cpp|cxx|h|hh|hpp|hxx)$', re.I)
 
